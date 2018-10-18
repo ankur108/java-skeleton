aa9217997997332eba0e133f2af789c0d3488ed5
Ankur Chaudhary <39953470+ankur108@users.noreply.github.com>
2018-10-19 00:42:00 +0100

    Update src/main/java/answers/Question1.java
    Question 1 Done

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -1,10 +1,22 @@
 package answers;
 
+import java.util.*;
+import java.util.Collections;
+
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-		return -1;
+            
+         ArrayList<Integer> FinalPortfolio = new ArrayList<Integer>();
+            
+         for(int j=0;j<portfolios.length;j++)
+            {
+               for(int k=j+1;k<portfolios.length;k++)
+               {
+                   FinalPortfolio.add(portfolios[j]^portfolios[k]);
+               }
+            }
+            return Collections.max(FinalPortfolio);
 	}
 
 }
