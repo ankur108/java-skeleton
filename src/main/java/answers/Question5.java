0c7f641471ae03b540682461bcb7cb6af150e6a9
Ankur Chaudhary <39953470+ankur108@users.noreply.github.com>
2018-10-20 00:27:02 +0100

    Update src/main/java/answers/Question5.java
    Question 5 Done!

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -1,10 +1,23 @@
 package answers;
 
+import java.util.Arrays;
+
 public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
-		// TODO Auto-generated method stub
-		return -1;
+		
+            int allocation=0;
+            
+            Arrays.sort(allowedAllocations); 
+            
+            for(int i=0;i<allowedAllocations.length;i++)
+            {
+                if(totalValue!=allowedAllocations[i] && totalValue%allowedAllocations[i]==0)
+                {
+                    allocation=totalValue/allowedAllocations[i];
+                }
+            }
+            return allocation;
 	}
 
 }
