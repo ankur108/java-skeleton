c647a9ed3a477d6d71a061dfe234e7fca834371b
Ankur Chaudhary <39953470+ankur108@users.noreply.github.com>
2018-10-21 00:59:40 +0100

    Update src/main/java/answers/Question5.java

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -6,18 +6,43 @@ public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
 		
-            int allocation=0;
+            int allocationA=0;
+            int allocationB=1;
+            int tempValue=totalValue;
             
             Arrays.sort(allowedAllocations); 
             
             for(int i=0;i<allowedAllocations.length;i++)
             {
-                if(totalValue!=allowedAllocations[i] && totalValue%allowedAllocations[i]==0)
+                if(totalValue%allowedAllocations[i]==0)
                 {
-                    allocation=totalValue/allowedAllocations[i];
+                    allocationA=totalValue/allowedAllocations[i];
                 }
             }
-            return allocation;
+            for(int i=allowedAllocations.length-1;i>=0;i--)
+            {
+                 if(i==allowedAllocations.length-1)
+                 {
+                     tempValue-=allowedAllocations[i];
+                 }
+                 
+                if(tempValue>=allowedAllocations[i])
+                {
+                  tempValue-=allowedAllocations[i];
+                  allocationB++;
+                  i--;
+                }
+                
+            }
+            
+            if(allocationB==1)
+            {
+               return allocationA; 
+            }
+            else
+            {
+               return allocationA>allocationB?allocationB:allocationA; 
+            }
 	}
 
 }
