69ec0b9500e0e619d8ee1018641e81c75f8a2a65
Ankur Chaudhary <39953470+ankur108@users.noreply.github.com>
2018-10-26 13:46:04 +0100

    Update src/main/java/answers/Question6.java
    Question 6 Done

--- src/main/java/answers/Question6.java
+++ src/main/java/answers/Question6.java
@@ -1,10 +1,58 @@
 package answers;
 
-public class Question6 {
 
+public class Question6 {
+    
+    public static int minDistance(int dist[], Boolean sptSet[],int numServers) 
+	{ 
+		int min = Integer.MAX_VALUE, min_index=-1; 
+
+		for (int v = 0; v < numServers; v++) 
+			if (sptSet[v] == false && dist[v] <= min) 
+			{ 
+				min = dist[v]; 
+				min_index = v; 
+			} 
+
+		return min_index; 
+	}
+      public static int printSolution(int dist[], int n) 
+	{ 
+	    return dist[0]; 
+	}
+   
 	public static int shortestServerRoute(int numServers, int targetServer, int[][] times) {
-		// TODO Auto-generated method stub
-		return -1;
+		
+        Question6 t = new Question6(); 
+	int dist[] = new int[numServers]; 
+	Boolean sptSet[] = new Boolean[numServers]; 
+
+        if(targetServer>numServers)
+        {
+            return 0;
+        }
+        else
+        {
+            for (int i = 0; i < numServers; i++) 
+            { 
+		dist[i] = Integer.MAX_VALUE; 
+		sptSet[i] = false; 
+	    } 
+
+	dist[targetServer] = 0; 
+
+        for (int count = 0; count < numServers-1; count++) 
+            { 
+                int u = minDistance(dist, sptSet,numServers);
+		sptSet[u] = true; 
+		for (int v = 0; v < numServers; v++) 
+        		if (!sptSet[v] && times[u][v]!=0 && dist[u] != Integer.MAX_VALUE && dist[u]+times[u][v] < dist[v]) 
+					dist[v] = dist[u] + times[u][v]; 
+		} 
+
+            return printSolution(dist, numServers);      
+        }
+        
 	}
 
 }
