e86ba0f943dd123aa2dad7a11c623a980d75820d
Ankur Chaudhary <39953470+ankur108@users.noreply.github.com>
2018-10-30 16:14:47 +0000

    Update src/main/java/answers/Question4.java

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -6,97 +6,41 @@ public class Question4 {
 
 	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
 		
-            ArrayList<Integer> sum = new ArrayList<Integer>(); 
-            ArrayList<Integer> sumAll = new ArrayList<Integer>(); 
-            
-    int summ=0;
-    int con=0;
-    int num=0;
-    int max=0;
-
-    out:for (int x = 0; x < rows.length; x++) 
-    {
-    in:for (int y = 0; y < rows.length; y++) 
-    {
-            if(rows[x][y].equals("X"))
-            {
-                if(con==1)
-                {
-                    if(num==numberMachines)
-                     {
-                        sum.add(summ);
-                      }
-                    else
-                    {
-                          sum.add(0); 
-                          
-                    }
-                    num=0;
-                    con=0;
-                    summ=0;
-                    continue out;
-                }
-                    
-            }
-            else
-            { num++;
-              summ+=Integer.parseInt(rows[x][y]); 
-              con=1;
-            }
+      int count=0;
+      int sum=0;
+      int temp=0;
+      int max=Integer.MAX_VALUE;
+      boolean X=true;
         
-    }
-    if(num==numberMachines)
-    {
-      sum.add(summ); 
-    }
-    else
-    {
-      sum.add(0);  
-    }
-    
-    
-    summ=0;
-    con=0;
-    num=0;
-    
-}
- for (int x = 0; x < rows.length; x++) 
-    {
-    for (int y = 0; y < rows.length; y++) 
-    {
-           try{
-               if(Integer.parseInt(rows[x][y])>-1)
-            {
-                    summ+=Integer.parseInt(rows[x][y]);                 
-            }
-           }
-           catch(Exception e){
-               
-           }
-    }  
-    sumAll.add(summ);
-    summ=0;
-}
- for (int x = 0; x < rows.length; x++) 
-    {
-    for (int y = 0; y < rows.length; y++) 
-    {
-        if(sum.get(x)==0)
-        {
-            continue;
-        }
-        else if(x==y)
-        {
-           max=sum.get(x)>sumAll.get(y)? sumAll.get(y):sum.get(x);
-        }
-        else
-        {
-            continue;
-        }
-    }  
-    
-}
-		return max;
+      
+      for(int i=0;i<rows.length;i++)
+      {
+          for(int j=0;j<rows.length;j++)
+          {
+              if(!(rows[i][j].equals("X")) && X && count<=numberMachines-1)
+              {
+                  temp+=Integer.parseInt(rows[i][j]);
+                  count++;
+                  if(count==numberMachines)
+                  {
+                      sum=temp;
+                  }
+              }
+              else
+              {
+                  if(count>0)
+                  {X=false;
+                  count=0;}
+              }
+          }
+          
+          X=true;
+          max=max>sum?sum:max;
+          temp=0;
+      }
+      
+      return max;
+      
 	}
 
 }
