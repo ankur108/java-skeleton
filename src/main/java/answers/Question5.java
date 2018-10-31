ace8b51f571765f8e73d0c27407537f9878f5efd
Ankur Chaudhary <39953470+ankur108@users.noreply.github.com>
2018-10-31 00:06:24 +0000

    Update src/main/java/answers/Question5.java

--- src/main/java/answers/Question5.java
+++ src/main/java/answers/Question5.java
@@ -6,43 +6,49 @@ public class Question5 {
 
 	public static int shareExchange(int[] allowedAllocations, int totalValue) {
 		
+            if(allowedAllocations.length<1)
+            {
+                return 0; 
+            }
+            else
+            {
             int allocationA=0;
             int allocationB=1;
             int tempValue=totalValue;
             
             Arrays.sort(allowedAllocations); 
             
-            for(int i=0;i<allowedAllocations.length;i++)
+            for(int i=allowedAllocations.length-1;i>=0;i--)
             {
                 if(totalValue%allowedAllocations[i]==0)
                 {
                     allocationA=totalValue/allowedAllocations[i];
+                    break;
                 }
+                
             }
+            
+            tempValue=allowedAllocations[allowedAllocations.length-1];
+
             for(int i=allowedAllocations.length-1;i>=0;i--)
             {
-                 if(i==allowedAllocations.length-1)
-                 {
-                     tempValue-=allowedAllocations[i];
-                 }
-                 
-                if(tempValue>=allowedAllocations[i])
+                if(tempValue>allowedAllocations[i])
                 {
                   tempValue-=allowedAllocations[i];
                   allocationB++;
-                  i--;
                 }
                 
             }
             
             if(allocationB==1)
             {
-               return allocationA; 
+                return allocationA; 
             }
             else
             {
-               return allocationA>allocationB?allocationB:allocationA; 
+                return allocationA>allocationB?allocationB:allocationA; 
+            }  
             }
-	}
+        }
 
 }
